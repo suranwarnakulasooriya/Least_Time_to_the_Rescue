208b04e64b8bf3dc5aaaa39643bb603be47d19b3
Suran Warnakulasooriya <68828123+suranwarnakulasooriya@users.noreply.github.com>
2023-06-16 12:40:55 -0400

    Add files via upload

--- snell.py
+++ snell.py
@@ -1,88 +1,79 @@
-from sympy.solvers import solve
-from sympy import Symbol
-from math import atan, pi
+from sympy.solvers import solve # solve quartic equation for x
+from sympy import Symbol # symbol class for the library to recognise x
+from math import atan, pi # arctan to find the angles and pi to convert to degrees
 import matplotlib.pyplot as plt
 
-x = Symbol('x')
-#Hs = Symbol('Hs')
-#Hw = Symbol('Hw')
-#l = Symbol('l')
-Vs, Vw = 8.8, 2.9
+x = Symbol('x') # horizontal distance the lifeguard must travel before entering the water
+Vs, Vw = 8.8, 2.9 # speed in sand and speed in water
 
-Ds = 30
-Dw = 150
-l = 0
+Ds = 30 # distance from the lifeguard to the beach (0 <= Ds <= 30)
+Dw = 150 # distance from the beach to the target (0 <= Dw <= 150)
+l = 0 # total horizontal distance between the lifeguard and target
 
-def find_T(x,Hs,Hw,l):
+def find_T(x,Hs,Hw,l): # find the time required to traverse a given path specified by x, Hs, Hw, and l
 	return (((Hs**2)+(x**2))**0.5)/Vs + (((Hw**2)+((l-x)**2))**0.5)/Vw
 
-
-def calculate(l,Hs,Hw,Vs,Vw):
-	V = Vw/Vs
+def calculate(l,Hs,Hw,Vs,Vw): # with given values, find x, the angles of refraction, time to traverse the refracted path, and time to traverse the straight path
+	V = Vw/Vs # the ratio between the speeds occurs often within the quartic equation for x, so it is calculated once separately
+	# use sympy.solvers to solve the quartic equation for x
 	S = solve((x**4)*(1-V**2) - 2*l*(x**3)*(1-V**2) + (x**2)*(Hs**2-(V**2)*(Hw**2)+(l**2)*(1-V**2)) - 2*l*(Hs**2)*x + (Hs**2)*(l**2), x)
-	#print(l,S)
-	best_T = 9e18
-	best_x = 0
-	for s in S:
+	# solve can yield multiple real positive solutions so a candidate test is needed
+	best_T = 9e18 # set a ludicrously high best time so that it is guaranteed to be reduced
+	best_x = 0 # set a throwaway best x candidate
+	for s in S: # for each candidate solution s in the list of solutions ...
 		try:
 			float(s)
-			if s >= 0:
-				candidate_T = find_T(s,Hs,Hw,l)
-				if candidate_T < best_T:
+			if s >= 0: # check if s is a real positive number
+				candidate_T = find_T(s,Hs,Hw,l) # find the time using the candidate s
+				if candidate_T < best_T: # set the best time and best x if the candidate s yields a faster time than the current best
 					best_T = candidate_T
 					best_x = s
 		except: pass
 
-	if best_T != 9e18:
-		X = best_x
-		T = best_T
+	if best_T != 9e18: # if there was a valid solution, best_T would be set to something other than 9e18, so if it is not 9e18, a valid solution was found
+		X = best_x; T = best_T # set the optimal horizontal distance and time 
 
-		theta_s = atan(X/Hs)*180/pi
-		theta_w = atan((l-X)/Hw)*180/pi
+		theta_s = atan(X/Hs)*180/pi; theta_w = atan((l-X)/Hw)*180/pi # find the angles that the lifeguard enters and leaves the beach from the normal in degrees
 
-		#T = (((Hs**2)+(X**2))**0.5)/Vs + (((Hw**2)+((l-X)**2))**0.5)/Vw
-
-		y = (Hs*l)/(Hs+Hw)
-		#T_prime = (((Hs**2)+(y**2))**0.5)/Vs + (((Hw**2)+((l-y)**2))**0.5)/Vw
-		T_prime = find_T(y,Hs,Hw,l)
+		y = (Hs*l)/(Hs+Hw) # find horizontal distance to the beach if the path was straight
+		T_prime = find_T(y,Hs,Hw,l) # find the time to traverse the straight path
 
 		return round(X,3), round(theta_s,3), round(theta_w,3), round(T,3), round(T_prime,3)
-	return None, None, None, None, None
-
-
-#print(calculate(l,Hs,Hw,Vs,Vw))
+	return None, None, None, None, None # if best_T is still 9e18, then no valid solutions were found
 
 ind = []
 dep1 = []
 dep2 = []
 
-#print(
-#find_T(59.41635475,Ds,Dw,5))
-
-for l in range(0,160,5):
-	#print(l,end=' ')
-	_x, _theta_s, _theta_w, t, t_prime = calculate(l,Ds,Dw,Vs,Vw)
-	if _x != None:
-		ind.append(l)
-		dep1.append(t)
+print('x values that could not be calculated:',end=' ')
+for l in range(0,150,5): # loop through various total horizontal distances
+	_x, _theta_s, _theta_w, t, t_prime = calculate(l,Ds,Dw,Vs,Vw) # calculate values with the given l
+	# somtimes solve does not return valid solutions for l values that should have a solution, those l values are skipped
+	if _x != None: # if the distance was valid
+		ind.append(l) # append l to the list of values for the independent variable
+		dep1.append(t) # append t and t_prime to the lists for the dependent variables
 		dep2.append(t_prime)
-		#print(l,_x,t,t_prime)
-	else: print(l)
-	#print(ind,dep1,dep2)
+	else: print(l,end=', ') # print any l values that yeiled no valid solutions
+print()
 
-#plt.plot(ind,dep1,label='$T_{Optimal}$')
-#plt.plot(ind,dep2,label='$T_{Straight}$')
+# choose whether to display both the optimal and straight paths (comparison) or just their difference (delta)
+display = 'comparison' # comparison or delta
 
-dep3 = []
-for i in range(len(dep1)):
-	dep3.append(dep2[i]-dep1[i])
-plt.plot(ind,dep3,label='Difference Between Times for Optimal Path and Straight-Line Path')
+# set up the plot for the given setting
+if display == 'comparison': 
+    plt.title(f'Times for Optimal Path and Straight-Line Path\nDs = {Ds} ft , Dw = {Dw} ft')
+    plt.ylabel('Time (s)')
+    plt.plot(ind,dep1,label='$T_{Optimal}$')
+    plt.plot(ind,dep2,label='$T_{Straight}$')
+elif display == 'delta':
+	plt.title(f'Difference Between Times for Optimal Path and Straight-Line Path\nDs = {Ds} ft , Dw = {Dw} ft')
+	plt.ylabel('∆T (s)')
+	dep3 = []
+	for i in range(len(dep1)): # create list of delta values
+		dep3.append(dep2[i]-dep1[i])
+	plt.plot(ind,dep3,label='Difference Between Times for Optimal Path and Straight-Line Path')
 
-#plt.title('Times for Optimal Path and Straight-Line Path\n$_{Ds = 30 ft , Dw = 150 ft}$')
-plt.title('Difference Between Times for Optimal Path and Straight-Line Path\n$_{Ds = 30 ft , Dw = 150 ft}$')
-plt.ylabel('∆T (s)')
 plt.xlabel('Horizontal Distance ($l$) in ft')
-#plt.legend()
 
 plt.show()
 plt.clf()
